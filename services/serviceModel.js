267c318f722963719119b505253eedb001acef5d
Tala Hamad <128089053+talahamad1@users.noreply.github.com>
2023-03-23 14:40:55 +0200

    AR-36 resolve comments

--- services/serviceModel.js
+++ services/serviceModel.js
@@ -1,13 +1,13 @@
-const Discount = require('../models/discountModel');
-module.exports.getDiscount = async () => {
-    return await Discount.find().populate('value', 'value -_id')
+const discount = require('../models/discountModel');
+module.exports.getDiscounts = async () => {
+    return await discount.find().populate('value');
 };
 module.exports.createDiscount = async newDiscount => {
-    return await Discount.create(newDiscount);
+    return await discount.create(newDiscount);
 };
 module.exports.deleteDiscount = async _id => {
-    return await Discount.deleteOne({ _id });
+    return await discount.deleteOne({ _id });
 };
 module.exports.updateById = async (id, newDiscount) => {
-    return await Discount.findByIdAndUpdate(id, newDiscount, { new: true }).populate('value');
+    return await discount.findByIdAndUpdate(id, newDiscount, { new: true }).populate('value');
 };
