d3de3748a49258d1efe288ad470efd7256b51460
Momena-zaghal <76658126+Momena-zaghal@users.noreply.github.com>
2023-03-28 02:13:28 +0300

    AR-13-create-the-controller.js-file

--- controller/userController.js
+++ controller/userController.js
@@ -1,40 +1,42 @@
 const User = require('../models/userModel');
 const userService = require('../service/userService');
 
-module.exports.createuser = async (req = express.request,res = express.response) =>{
+module.exports.createUser = async (req = express.request,res = express.response) =>{
     try{
     let user = new user(req.body);
-        pro.save();
+        user.save();
         res.status(200).json(user);
-    }catch (err) {
+    }
+    catch (err) {
         const error = `Failed to create user, error: ${err}`;
 		res.status(400).json({ error });
     }
 };
-module.exports.getuser = async (req = express.request,res = express.response) =>{
+module.exports.getUser = async (req = express.request,res = express.response) =>{
     try {
-		const user = await userService.getuser();
+		const user = await userService.getUser();
 		res.status(200).json(user);
-	} catch (err) {
+	} 
+    catch (err) {
 		const error = `Failed to get user, error: ${err}`;
 		res.status(400).json({ error });
 	}
 };
-module.exports.updateuser = async (req = express.request,res = express.response) =>{
+module.exports.updateUser = async (req = express.request,res = express.response) =>{
     const id = req.params.id;
-    const newInformation = req.body;
+    const updateFields = req.body;
 	try {
-        userService.updateuser(userId, newInformation);
+        userService.updateUser(userId, updateFields);
     }
     catch(err){
         const errors = `FAILD to Update user with id ${id}, err: ${error}`;
 		res.status(400).json({ errors});
     }
 };
-module.exports.deleteuser = async (req = express.request,res = express.response) =>{
+module.exports.deleteUser = async (req = express.request,res = express.response) =>{
     try{
         const id = req.params.id;
-        await userService.deleteuser(id);
+        await userService.deleteUser(id);
         res.status(204);
     }
     catch(err){
@@ -42,3 +44,4 @@ module.exports.deleteuser = async (req = express.request,res = express.response)
 		res.status(400).json({ errors});
     }
 };
+
