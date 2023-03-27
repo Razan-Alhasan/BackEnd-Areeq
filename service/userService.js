c980cdc44815beaaa2a0f86de1f3e5bfca3444fd
Momena-zaghal <76658126+Momena-zaghal@users.noreply.github.com>
2023-03-28 01:48:14 +0300

    AR-12-create-the-Service.js-file

--- service/userService.js
+++ service/userService.js
@@ -1,21 +1,26 @@
+const { default: mongoose } = require('mongoose');
 const User = require('../models/userModel');
 
 module.exports.createUser = async userData => {
 	return await User.create(userData);
 };
-module.exports.updateUser = async (id, newInformation) => {
-	return await User.findByIdAndUpdate(id, newInformation, { new: true });
+module.exports.updateUser = async (id, updateFields) => {
+	return await User.findByIdAndUpdate(id, updateFields, { new: true });
 };
 module.exports.removeUser = async user_id => {
-	await Order.deleteMany({ user: _id });
-	return await User.deleteOne({ _id });
+	removeUser.pre('deleteOne', function(){
+		this instanceof mongoose.Query;
+	});
+	removeUser.pre('deleteMany', function(){
+		this instanceof mongoose.Query;
+	});
 };
 module.exports.getUserById = async () => {
 	const user = await User.findById({ _id: id });
     delete user.password;
     return user; 
 };
-module.exports.getallusers = () =>{
+module.exports.getAllUsers = () =>{
 const usersWithoutPassword = users.map(user => {
 	const { password, ...userWithoutPassword } = user.toObject();
 	return userWithoutPassword;
