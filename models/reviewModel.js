d9246aed4f6342ce74528d221c39c27094ac309f
razan alhasan <alhasanrazan6@gmail.com>
2023-03-28 20:06:34 +0300

    AR-16/create the model.js file

---
+++ models/reviewModel.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+
+const reviewSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'please add userId']
+    },
+    product:{
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+        required: [true, 'please add productId']
+    },
+    rate:{ 
+        type:Number,
+        min:0,
+        max:5,
+        default:0
+    },
+    description:{
+        type:String,
+        required:[true, 'please add your review'],
+    }
+},
+    { timestamps: {
+        createdAt: 'created_at', 
+        updatedAt: 'updated_at' }
+    }
+);
+const review = model('Review', reviewSchema);
+module.exports = review;
