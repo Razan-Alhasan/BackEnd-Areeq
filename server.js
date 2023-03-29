74fbfb588c1cc7264a06972fcdcae034703ac340
Razan Alhasan <115662722+Razan-Alhasan@users.noreply.github.com>
2023-03-29 16:43:03 +0300

    AR-2/create routes for product (#4)
    * AR-3/create the models.js file
    
    * AR-3/ resolve comments
    
    * AR-5: create the service.js file (#9)
    
    * AR-5: create the service.js file
    
    * AR-5 : resolve comments
    
    ---------
    
    Co-authored-by: razan alhasan <alhasanrazan6@gmail.com>
    
    * Ar 5/create the service.js file (#11)
    
    * AR-5: create the service.js file
    
    * AR-5 : resolve comments
    
    ---------
    
    Co-authored-by: razan alhasan <alhasanrazan6@gmail.com>
    
    * AR-4 and AR-29 : create controller.js and routes.js for product (#15)
    
    Co-authored-by: razan alhasan <alhasanrazan6@gmail.com>
    
    ---------
    
    Co-authored-by: razan alhasan <alhasanrazan6@gmail.com>

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const productRoute = require('./routes/productRoutes');
 const cors = require('cors');
-
 const connectDb = async() => {
     try{
         await mongoose.connect('mongodb://127.0.0.1:27017/areeq');
         console.log('connected to the database');
         return true;
     }catch(err){
-        console.log(`failed to connect with the database wiht err: ${err.message}`);
+        console.log(`failed to connect with the database with err: ${err.message}`);
         return false;
     }
 }
 
-
-
 connectDb();
-const { Schema } = mongoose;
-const orderSchema = new Schema(
-    {
-        user: String
-    });
 const app = express();
+app.use('/', productRoute);
 app.use(cors());
 app.use(express.json());
 app.listen(process.env.PORT | '3000');
