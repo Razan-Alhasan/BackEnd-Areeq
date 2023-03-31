a5ac4ae5a1d4b562c451ba8584da0850d7f271e2
Momena-zaghal <76658126+Momena-zaghal@users.noreply.github.com>
2023-03-31 14:39:06 +0300

    AR-12-create-the-Service.js-file

--- service/userService.js
+++ service/userService.js
@@ -5,12 +5,13 @@ module.exports.createUser = async userData => {
 	return await User.create(userData);
 };
 module.exports.updateUser = async (id, updateFields) => {
-	return await User.findByIdAndUpdate(id, updateFields, { new: true });
+	return await User.findByIdAndUpdate(id, {$set: updateFields}, { new: true });
 };
 module.exports.removeUser = async user_id => {
+	return await User.deleteOne({id});
 };
 module.exports.getUserById = async () => {
-	const user = await User.findById({ _id: id });
+	const user = await User.findById({id});
     delete user.password;
     return user; 
 };
