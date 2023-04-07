0134221da0358dfcd363134d7308826e0d5bb13b
Momena-zaghal <76658126+Momena-zaghal@users.noreply.github.com>
2023-04-07 14:44:55 +0300

    fixRoute

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { model } = require('mongoose');
+const {model} = require('mongoose');
 const router = express.Router();
 const userController = require('../controller/userController');
 
