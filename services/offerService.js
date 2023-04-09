2eda116f2e1b2db2d1b171d318fa66b1c4490f28
Momena-zaghal <76658126+Momena-zaghal@users.noreply.github.com>
2023-04-10 00:47:58 +0300

    AR-25-create-routes-for-offer

--- services/offerService.js
+++ services/offerService.js
@@ -9,7 +9,7 @@ module.exports.updateOffer = async (id, newInformation) => {
 module.exports.removeOffer = async Offer_id => {
 	return await offer.deleteOne({ _id : Offer_id });
 };
-module.exports.getOfferById = async (id ) => { 
+module.exports.getOfferById = async (id) => { 
 	return await offer.findById(id);
 };
 module.exports.getAllOffers = async () =>{
