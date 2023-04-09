312c51f11db14ccbf465facf5678149c9f0b87dd
Tala Hamad <128089053+talahamad1@users.noreply.github.com>
2023-04-10 01:16:35 +0300

    AR-40 resolve comments

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const productRoute = require('./routes/productRoutes');
+const discountRoute = require('./routes/discountRoutes');
 const cors = require('cors');
 const connectDb = async() => {
     try{
@@ -16,6 +17,7 @@ const connectDb = async() => {
 connectDb();
 const app = express();
 app.use('/', productRoute);
+app.use('/', discountRoute);
 app.use(cors());
 app.use(express.json());
 app.listen(process.env.PORT | '3000');
