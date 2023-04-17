bbf221177e3ee21e70ae307b5adb883fcdb2d738
Momena-zaghal <76658126+Momena-zaghal@users.noreply.github.com>
2023-04-17 18:29:34 +0300

    Revert "AR-45-fix-type-of-user (#31)"
    This reverts commit 2c83543d2521692f5d4c94ad6f68fe43e1ca9fa1.

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -6,12 +6,3 @@ module.exports = function authorizationCheck(req, res, next) {
       next();
     } 
   };
-
-  module.exports = function authorizationCheck(req, res, next) {
-    if (!req.user.isSeller) { 
-     return res.sendStatus(401);
-    } else {
-      // move to the next middleware, cause it's ok
-      next();
-    } 
-  };
